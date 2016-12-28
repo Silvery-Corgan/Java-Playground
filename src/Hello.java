a6daef957b53b3d371a306901a6600e04629b489
Sorin-Mihai Oprea <silvery.corgan@gmail.com>
2016-12-28 16:27:37 +0200

    Add println statement with newlines

--- src/Hello.java
+++ src/Hello.java
@@ -4,9 +4,10 @@ public class Hello
 	public static void main(String[] args)
 	{
 		// generate some simple output
-		System.out.println("Hello, World!");    // first line
-		System.out.println("How are you?");     // another line
-		System.out.print("Goodbye, ");          // this is a print statement that doesn't add up a new line
-		System.out.println("cruel world");      // this one adds up a new line after concatenating it with the previous string
+		System.out.println("Hello, World!");                // first line
+		System.out.println("How are you?");                 // another line
+		System.out.print("Goodbye, ");                      // this is a print statement that doesn't add up a new line
+		System.out.println("cruel world");                  // this one adds up a new line after concatenating it with the previous string
+		System.out.print("Hello!\nHow are you doing?\n");   // add new line manually
 	}
 }
