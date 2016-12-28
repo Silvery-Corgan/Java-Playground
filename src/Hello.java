089a798052bf7545c9c1b5790de3aeaf3898ff11
Sorin-Mihai Oprea <silvery.corgan@gmail.com>
2016-12-28 16:25:26 +0200

    Add print statement and println statement for showcasing the difference.

--- src/Hello.java
+++ src/Hello.java
@@ -6,5 +6,7 @@ public class Hello
 		// generate some simple output
 		System.out.println("Hello, World!");    // first line
 		System.out.println("How are you?");     // another line
+		System.out.print("Goodbye, ");          // this is a print statement that doesn't add up a new line
+		System.out.println("cruel world");      // this one adds up a new line after concatenating it with the previous string
 	}
 }
