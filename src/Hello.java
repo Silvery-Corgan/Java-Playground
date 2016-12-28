78252edef0204ad6a0188e1cecf587e42580ad11
Sorin-Mihai Oprea <silvery.corgan@gmail.com>
2016-12-28 20:24:05 +0200

    Declare & assign some vars

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,9 @@ public class Hello
 
 	public static void main(String[] args)
 	{
+		String message, firstName, lastName;
+		int hour, minute;
+
 		// generate some simple output
 		System.out.println("Hello, World!");                // first line
 		System.out.println("How are you?");                 // another line
@@ -11,5 +14,9 @@ public class Hello
 		System.out.print("Hello!\nHow are you doing?\n");   // add new line manually
 		System.out.println("She said \"Hello!\" to me.");   // add quotation marks
 		System.out.println("What's up doc?");               // add new statement
+
+		message = "Hello!";  // give message the value "Hello!"
+		hour = 11;           // assign the value 11 to hour
+		minute = 59;         // set minute to 59
 	}
 }
