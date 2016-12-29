a5a87957cec09e8fa3bac38294681e1347e0790f
Sorin-Mihai Oprea <silvery.corgan@gmail.com>
2016-12-29 21:53:51 +0200

    Calculate from midnight

--- src/Time.java
+++ src/Time.java
@@ -3,5 +3,7 @@ public class Time
 	public static void main(String[] args)
 	{
 		int hour=13, minute=57, second=30;
+		int midnighth=24, midnightm=60, midnights=60;
+		System.out.println("Since midnight, there have been " + (midnighth-hour) + " hours, " + (midnightm-minute) + " minutes, " + (midnights-second) + " seconds.");
 	}
 }
