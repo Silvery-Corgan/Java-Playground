a30a452d778e0716034666ba5c3eb971e6611703
Sorin-Mihai Oprea <silvery.corgan@gmail.com>
2016-12-29 13:43:35 +0200

    Add vars

--- src/Date.java
+++ src/Date.java
@@ -2,6 +2,11 @@ public class Date
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Test");
+		int date=29, year=2016;
+		String day="Thursday", month="December";
+		System.out.println(day);
+		System.out.println(month);
+		System.out.println(date);
+		System.out.println(year);
 	}
 }
