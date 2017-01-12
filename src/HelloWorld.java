3828afc9c8b003a916d5e0c16573150143074491
Sorin-Mihai Oprea <silvery.corgan@gmail.com>
2017-01-12 10:24:33 +0100

    Fix formatting

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,19 +1,22 @@
 /**
- * This is a ... "Hello World" Example
+ * This is a ... peculiar ... "Hello World" Example
  */
 
 import java.util.*;
 
 public class HelloWorld
 {
-	public static void main(String ... args) {
-		System.out.println(randomString(-229985452)+' '+randomString(-147909649));
+	public static void main(String... args)
+	{
+		System.out.println(randomString(-229985452) + ' ' + randomString(-147909649));
 	}
 
-	public static String randomString(int seed) {
+	public static String randomString(int seed)
+	{
 		Random rand = new Random(seed);
 		StringBuilder sb = new StringBuilder();
-		for(int i=0;;i++) {
+		for (int i = 0; ; i++)
+		{
 			int n = rand.nextInt(27);
 			if (n == 0) break;
 			sb.append((char) ('`' + n));
