37b437811dfffcd19ee36c372eeac02d436c73db
Sorin-Mihai Oprea <silvery.corgan@gmail.com>
2017-06-02 22:08:28 +0200

    Change default template

---
+++ src/Book/ComputeAverage/ComputeAverage.java
@@ -0,0 +1,28 @@
+package Book.ComputeAverage;
+
+import java.util.Scanner;
+
+/**
+ * @author Sorin-Mihai Oprea
+ * @date 2017-06-02
+ */
+public class ComputeAverage
+{
+	public static void main(String[] args)
+	{
+		//Create a Scanner Object
+		Scanner input = new Scanner(System.in);
+
+		//Prompt the user to enter three numbers
+		System.out.println("Enter three numbers: ");
+		double num1 = input.nextDouble();
+		double num2 = input.nextDouble();
+		double num3 = input.nextDouble();
+
+		//Compute Average
+		double average = (num1 + num2 + num3) / 3;
+
+		//Display Result
+		System.out.println("Average of " + num1 + " " + num2 + " " + num3 + " is: " + average);
+	}
+}
